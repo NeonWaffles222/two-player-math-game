7615c6a4b0f1dc5470d74afeb2279c7dd31171dc
Noah McMillan <noahm27@gmail.com>
2023-09-19 14:42:54 -0700

    changed the random number to select the correct range of numbers

--- math.rb
+++ math.rb
@@ -5,8 +5,8 @@ class Question
   end
 
   def question
-    num1 = rand(11)
-    num2 = rand(11)
+    num1 = rand(1..20)
+    num2 = rand(1..20)
     puts "What does #{num1} plus #{num2} equal?"
     @answer = num1 + num2
   end
